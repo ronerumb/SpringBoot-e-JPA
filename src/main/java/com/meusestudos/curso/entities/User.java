9f2262d6650affa7c21f306ac589f729feb97fd4
Roner <ronerumb@gmail.com>
2023-01-04 19:45:48 -0300

    H2 database

--- src/main/java/com/meusestudos/curso/entities/User.java
+++ src/main/java/com/meusestudos/curso/entities/User.java
@@ -3,10 +3,20 @@ package com.meusestudos.curso.entities;
 import java.io.Serializable;
 import java.util.Objects;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity // identifica que é uma entity
+@Table(name = "tb_user")
 public class User implements Serializable {
 
 	private static final long serialVersionUID = 1L;
-
+	
+	@Id//mostra que o id será a chave primaria
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String name;
 	private String email;
