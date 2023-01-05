7028a9702b6994bb13ae51608a76ff9e5e9e68a6
Roner <ronerumb@gmail.com>
2023-01-05 20:31:13 -0300

    Product-OrderItem one-to-many association

--- src/main/java/com/meusestudos/curso/entities/Product.java
+++ src/main/java/com/meusestudos/curso/entities/Product.java
@@ -5,6 +5,8 @@ import java.util.HashSet;
 import java.util.Objects;
 import java.util.Set;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -12,6 +14,7 @@ import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.JoinTable;
 import jakarta.persistence.ManyToMany;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 
 @Entity
@@ -28,18 +31,16 @@ public class Product implements Serializable {
 	private String description;
 	private Double price;
 	private String imgUrl;
-	
+
 	@ManyToMany
-	@JoinTable(name = "tb_product_category",
-	joinColumns = @JoinColumn(name = "product_id"),
-	inverseJoinColumns = @JoinColumn(name = "category_id"))
-	private Set<Category> categories = new HashSet<>();//utilizado set para não repetir a categoria
-	
+	@JoinTable(name = "tb_product_category", joinColumns = @JoinColumn(name = "product_id"), inverseJoinColumns = @JoinColumn(name = "category_id"))
+	private Set<Category> categories = new HashSet<>();// utilizado set para não repetir a categoria
+	@OneToMany(mappedBy = "id.product")
+	private Set<OrderItem> items = new HashSet<>();
+
 	public Product() {
-		
+
 	}
-	
-	
 
 	public Product(Integer id, String name, String description, Double price, String imgUrl) {
 		super();
@@ -49,8 +50,6 @@ public class Product implements Serializable {
 		this.price = price;
 		this.imgUrl = imgUrl;
 	}
-
-
 
 	public Integer getId() {
 		return id;
@@ -96,6 +95,14 @@ public class Product implements Serializable {
 	public int hashCode() {
 		return Objects.hash(id);
 	}
+	@JsonIgnore
+	public Set<Order> getOrders() {
+		Set<Order> set = new HashSet<>();
+		for (OrderItem x : items) {
+			set.add(x.getOrder());
+		}
+		return set;
+	}
 
 	@Override
 	public boolean equals(Object obj) {
@@ -109,11 +116,8 @@ public class Product implements Serializable {
 		return Objects.equals(id, other.id);
 	}
 
-
-
 	public Set<Category> getCategories() {
 		return categories;
 	}
-	
 
 }
